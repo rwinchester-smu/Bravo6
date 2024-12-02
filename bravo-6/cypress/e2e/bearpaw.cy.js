b1d806d18b4203e4bd9b2e1e550ddce9c79493d5
rileywinchester <rileywinchester@gmail.com>
2024-12-02 14:53:34 -0400

    cypress e2e spec

--- bravo-6/cypress/e2e/bearpaw.cy.js
+++ bravo-6/cypress/e2e/bearpaw.cy.js
@@ -1,28 +1,13 @@
-describe('BearPaw Presence and Dragging Test', () => {
+describe('BearPaw Component Test by Riley Winchester', () => {
   const baseUrl = 'http://ugdev.cs.smu.ca:3002';
 
   beforeEach(() => {
-    // Visit the application
+    // Visit the application before each test
     cy.visit(baseUrl);
   });
 
   it('should confirm the presence of the BearPaw component', () => {
     // Check that the BearPaw element is visible
     cy.get('[data-cy="bearpaw"]').should('be.visible');
-  });
-
-  it('should allow the BearPaw component to be draggable', () => {
-    // Simulate dragging the BearPaw element
-    cy.get('[data-cy="bearpaw"]')
-      .trigger('mousedown', { which: 1 })
-      .trigger('mousemove', { clientX: 200, clientY: 200 })
-      .trigger('mouseup');
-    
-    // Optionally check the new position if draggable logic is implemented
-    cy.get('[data-cy="bearpaw"]').should(($bearpaw) => {
-      const position = $bearpaw.position();
-      expect(position.left).to.be.greaterThan(0);
-      expect(position.top).to.be.greaterThan(0);
-    });
   });
 });
