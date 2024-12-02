44797908641bc5f3bcd5da4b7acdbf33ace15fb4
rileywinchester <rileywinchester@gmail.com>
2024-12-02 14:55:51 -0400

    e2e spec

--- bravo-6/cypress/e2e/bearpaw-spec.cy.js
+++ bravo-6/cypress/e2e/bearpaw-spec.cy.js
@@ -2,12 +2,10 @@ describe('BearPaw Component Test by Riley Winchester', () => {
   const baseUrl = 'http://ugdev.cs.smu.ca:3002';
 
   beforeEach(() => {
-    // Visit the application before each test
     cy.visit(baseUrl);
   });
 
   it('should confirm the presence of the BearPaw component', () => {
-    // Check that the BearPaw element is visible
     cy.get('[data-cy="bearpaw"]').should('be.visible');
   });
 });
