d9f9976dc2c51ae7a1226d620b846d12ed853d94
Sam <Samuel.Kohler@smu.ca>
2024-12-02 21:06:03 -0400

    a

--- bravo-6/cypress/e2e/dictionary-spec.cy.js
+++ bravo-6/cypress/e2e/dictionary-spec.cy.js
@@ -1,2 +1,8 @@
 // Sam's test - dictionary closes properly
-//new code
+describe('Exit Dictionary Button Test by Sam Kohler', () => {
+    it("should exit the dictionary",()=>{
+        cy.visit("http://ugdev.cs.smu.ca:3002");
+        cy.get("button").click();
+        cy.get("h1").not("dictionary");
+    });
+});
